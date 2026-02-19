e53431d48f0dec13d6641b21abce4d84506ec35e
VERT Quentin <qvert@pouchain.fr>
2026-02-19 14:47:30 +0100

    Update worker.js: Allow Authorization header

--- worker.js
+++ worker.js
@@ -16,7 +16,7 @@ export default {
         const corsHeaders = {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type, x-worker-secret",
+            "Access-Control-Allow-Headers": "Content-Type, x-worker-secret, Authorization",
         };
 
         // 1. Handle Preflight (OPTIONS)
